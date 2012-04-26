ca7db51b880591e76da46bacf84c1da279a7a59d
Mark Vasilkov <mvasilkov@gmail.com>
2012-04-26 09:55:17 +0300

    MySQL support

--- settings.php
+++ settings.php
@@ -4,4 +4,8 @@
 # Licensed under the GNU General Public License (GPL) Version 2
 define('sviter_username', 'mvasilkov');
 
+define('database_name', 'sviter');
+define('database_user', 'sviter');
+define('database_password', 'c-c-c-combo sviter');
+
 error_reporting(E_ALL & ~E_STRICT);
--- sviter-cli.php
+++ sviter-cli.php
@@ -2,6 +2,7 @@
 # This is Sviter command-line interface (CLI)
 # Programmed by Rei Ayanami, http://animuchan.net/
 # Licensed under the GNU General Public License (GPL) Version 2
+require_once('rb.php');
 require_once('simplepie.php');
 require_once('settings.php');
 
@@ -13,6 +14,25 @@ $feed->set_feed_url(api_url.sviter_username);
 $feed->init();
 $feed->handle_content_type();
 
+R::setup('mysql:host=localhost;dbname='.database_name, database_user, database_password);
+
+$unique = array(array('link'));
+$new_rec = 0;
+
 foreach ($feed->get_items() as $item) {
-	printf("---\n%s\n%s\n", $item->get_title(), $item->get_permalink());
+	$obj = R::dispense('sviter_post');
+
+	$obj->text = $item->get_title();
+	$obj->link = $item->get_permalink();
+
+	$obj->setMeta('buildcommand.unique', $unique);
+
+	try {
+		R::store($obj);
+		++$new_rec;
+	}
+	catch (Exception $not_used) {
+	}
 }
+
+echo "New records: $new_rec\n";
